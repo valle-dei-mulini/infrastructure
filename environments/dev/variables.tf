8525d00b6e431c817b4c755528c8db53cecf112d
rufusnufus <nufusrufus@gmail.com>
2021-07-14 10:22:20 +0300

    delete apps

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -52,7 +52,7 @@ variable "domain_name" {
 variable "argocd" {
   default = {
     repository = "infrastructure"
-    branch     = "main"
+    branch     = "master"
     owner      = "valle-dei-mulini"
   }
   type        = map(string)
